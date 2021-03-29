1e24d2212dca720bcf5c927aad3ae48d36244f51
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-03-29 08:01:36 +0800

    Swagger 設置

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8083, () => {
     Logger.log('Content Service is running on port 8083!');
+  initSwagger(app);
   });
 }
 bootstrap();
