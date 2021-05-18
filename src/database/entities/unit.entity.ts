3a4cc1233127314248f0a27615d69dc0089130ad
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-05-19 05:04:28 +0800

    更新 unit entity

--- src/database/entities/unit.entity.ts
+++ src/database/entities/unit.entity.ts
@@ -1,6 +1,7 @@
-import { Entity, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, OneToMany, JoinTable } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { Lecture } from './lecture.entity';
+import { Tag } from './tag.entity';
 
 @Entity()
 export class Unit extends BaseEntity {
@@ -9,6 +10,22 @@ export class Unit extends BaseEntity {
 
   @Column({ type: 'varchar', nullable: false })
   title: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  youtube_id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  description: string;
+
+  @OneToMany(
+    type => Tag,
+    tag => tag.unit,
+    {
+      eager: false,
+    },
+  )
+  @JoinTable()
+  tags: Tag[];
 
   @ManyToOne(
     type => Lecture,
