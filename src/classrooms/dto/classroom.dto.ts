8930b53dd09297b376dd0be6fdb79f84c82575bf
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-06-02 11:57:05 +0800

    新增 classroom dto

--- src/classrooms/dto/classroom.dto.ts
+++ src/classrooms/dto/classroom.dto.ts
@@ -1,7 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsBoolean, Length } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsBoolean,
+  Length,
+  IsUUID,
+} from 'class-validator';
 import { Classroom } from 'src/database/entities/classroom.entity';
-export class CreateClassroomDto {
+import { StudentGroup } from 'src/database/entities/studentGroup.entity';
+
+export class ClassroomDto {
+  @ApiProperty({ required: true })
+  @IsUUID('all', { message: '習題型態錯誤' })
+  id: string;
+
   @ApiProperty({ required: true })
   @Length(1, 50, { message: '班級名稱需要小於 50' })
   @IsString({ message: '班級名稱型態錯誤' })
@@ -17,37 +29,38 @@ export class CreateClassroomDto {
   @IsBoolean({ message: '是否開放學生加入型態錯誤' })
   isAllowAdd: boolean;
 
+  @ApiProperty({ required: true })
+  @IsString({ message: '班級教師型態錯誤' })
+  @IsNotEmpty({ message: '班級教師不得為空' })
   manager: string;
 
-  // @ApiProperty({ required: true })
-  // @IsString({ message: '組別型態錯誤' })
-  // @IsNotEmpty({ message: '班級名稱不得為空' })
-  // groupList: string;
+  @ApiProperty({ required: false, nullable: true })
+  studentGroups: StudentGroup[];
 
-  // @ApiProperty({ required: true })
-  // @ValidateNested({ each: true })
-  // @Type(() => QuestionDto)
-  // questions: QuestionDto[];
+  @ApiProperty({ required: false, nullable: true })
+  studentList: string;
 
-  // @ApiProperty({ required: true })
-  // @IsString({ message: '班級學生型態錯誤' })
-  // @IsNotEmpty({ message: '班級學生不得為空' })
-  // studentList: string;
-
-  public static from(dto: Partial<CreateClassroomDto>) {
-    const it = new CreateClassroomDto();
+  public static from(dto: Partial<ClassroomDto>) {
+    const it = new ClassroomDto();
+    it.id = dto.id;
     it.name = dto.name;
     it.description = dto.description;
     it.isAllowAdd = dto.isAllowAdd;
+    it.manager = dto.manager;
+    it.studentGroups = dto.studentGroups;
+    it.studentList = dto.studentList;
     return it;
   }
 
   public static fromEntity(entity: Classroom) {
     return this.from({
+      id: entity.id,
       name: entity.name,
       description: entity.description,
       isAllowAdd: entity.isAllowAdd,
       manager: entity.manager,
+      studentGroups: entity.studentGroups,
+      studentList: entity.studentList,
     });
   }
 
